8192bf15cf3dc9bab2dd38b04bc071b9ded24e58
minji kim <rlaqufkr953@gmail.com>
2025-02-22 23:56:59 +0900

    to solve error to deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,20 +3,8 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
-// CONTEXT
-// import { RefContext } from "../context/refContext";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    {/* <RefContext.Provider
-      value={{
-        Home: useRef(null),
-        Projects: useRef(null),
-        About: useRef(null),
-        Contact: useRef(null),
-      }}
-    > */}
     <App />
-    {/* </RefContext.Provider> */}
   </StrictMode>
 );
