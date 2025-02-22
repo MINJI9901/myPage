d60a32bf9ebf67449d65846b5c29cd0c2fa678c3
minji kim <rlaqufkr953@gmail.com>
2025-02-22 23:53:33 +0900

    comment out import that occurs error to deploy

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App.jsx";
 
 // CONTEXT
-import { RefContext } from "../context/refContext";
+// import { RefContext } from "../context/refContext";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
