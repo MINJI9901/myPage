2f1cb356efa696644df043003932638f5609cd16
minji kim <rlaqufkr953@gmail.com>
2025-03-04 20:42:40 +0900

    add extra motion

--- src/components/about/SkillStack.jsx
+++ src/components/about/SkillStack.jsx
@@ -3,6 +3,8 @@ import { Box, Grid2, Typography } from "@mui/material";
 // COMPONENTS
 import LogoGrid from "../generic/LogoGrid";
 
+import AppearingMotion from "../generic/AppearingMotion";
+
 export default function SkillStack() {
   const frontendSkill = [
     "React",
@@ -17,26 +19,28 @@ export default function SkillStack() {
   const backendSkill = ["NodeJs", "ExpressJs", "MongoDB", "Supabase"];
 
   return (
-    <Box
-      display={{ xs: "block", sm: "flex" }}
-      justifyContent={"space-between"}
-      fontSize={"3rem"}
-      p={"2rem 0"}
-      textAlign={"center"}
-      gap={3}
-    >
-      <Box width={{ xs: "100%", md: "45%" }} mb={{ xs: "2rem", sm: 0 }}>
-        <Typography fontSize={"1.5rem"} fontWeight={700}>
-          Front-end
-        </Typography>
-        <LogoGrid logoNames={frontendSkill} displayName={true} />
+    <AppearingMotion>
+      <Box
+        display={{ xs: "block", sm: "flex" }}
+        justifyContent={"space-between"}
+        fontSize={"3rem"}
+        p={"2rem 0"}
+        textAlign={"center"}
+        gap={3}
+      >
+        <Box width={{ xs: "100%", md: "45%" }} mb={{ xs: "2rem", sm: 0 }}>
+          <Typography fontSize={"1.5rem"} fontWeight={700}>
+            Front-end
+          </Typography>
+          <LogoGrid logoNames={frontendSkill} displayName={true} />
+        </Box>
+        <Box width={{ xs: "100%", md: "45%" }}>
+          <Typography fontSize={"1.5rem"} fontWeight={700}>
+            Back-end
+          </Typography>
+          <LogoGrid logoNames={backendSkill} displayName={true} />
+        </Box>
       </Box>
-      <Box width={{ xs: "100%", md: "45%" }}>
-        <Typography fontSize={"1.5rem"} fontWeight={700}>
-          Back-end
-        </Typography>
-        <LogoGrid logoNames={backendSkill} displayName={true} />
-      </Box>
-    </Box>
+    </AppearingMotion>
   );
 }
